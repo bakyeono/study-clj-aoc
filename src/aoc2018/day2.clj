0c99d35a4837e50348292a8584c8e43857c8fbc3
bakyeono <bakyeono@gmail.com>
2021-10-26 10:33:19 +0900

    Solve aoc 2018 day 2

---
+++ src/aoc2018/day2.clj
@@ -0,0 +1,38 @@
+;; https://adventofcode.com/2018/day/2
+(ns aoc2018.day2
+  (:require [clojure.string :as string]
+            [clojure.math.combinatorics :as combinatorics]
+            [util.util :as util]))
+
+(defn parse-input [input]
+  (->> input
+       string/split-lines
+       (map #(string/split % #""))))
+
+(defn duplicated-n-times? [n coll]
+  (->> (frequencies coll)
+       (filter #(= (second %) n))
+       first))
+(def twice? (partial duplicated-n-times? 2))
+(def triple? (partial duplicated-n-times? 3))
+
+(defn common [[a b]]
+  (map #(when (= %1 %2) %1) a b))
+
+(def puzzle-input (slurp "data/aoc2018/day2.data"))
+
+;; solve part 1
+(->> puzzle-input
+     parse-input
+     (#(* (count (filter twice? %))
+          (count (filter triple? %)))))
+
+;; solve part 2
+(->> puzzle-input
+     parse-input
+     (#(combinatorics/combinations % 2))
+     (map common)
+     (filter #(= 1
+                 (count (filter nil? %))))
+     first
+     (apply str))
