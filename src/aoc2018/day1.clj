ad7211d94b8635fd79f6f517d5273a47a24cc928
Yeono Park <bakyeono@gmail.com>
2022-05-25 16:38:46 +0900

    Update day1.clj

--- src/aoc2018/day1.clj
+++ src/aoc2018/day1.clj
@@ -11,7 +11,7 @@
      (map #(Integer/parseInt %))
      (reduce +))
 
-(defn get-duplicated-or-store [seen-set value]
+(defn get-first-duplicated [seen-set value]
   (if (seen-set value)
     (reduced value)
     (conj seen-set value)))
@@ -22,4 +22,4 @@
      (map #(Integer/parseInt %))
      cycle
      (reductions +)
-     (reduce get-duplicated-or-store #{}))
+     (reduce get-first-duplicated #{}))
